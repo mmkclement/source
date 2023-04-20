f9278c247eeda34df2035033b192eab7b19b7435
mmkclement <maungmaungkha18290@gmail.com>
2023-04-20 10:52:53 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class AddForm extends React.Component {
+  nameRef = React.createRef(); priceRef = React.createRef();
+  add = () => {
+    let name = this.nameRef.current.value; let price = this.priceRef.current.value;
+    this.props.add(name, price);
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.nameRef} /><br />
+        <input type="text" ref={this.priceRef} /><br />
+        <button onClick={this.add}>Add</button>
+      </div>)
+  }
+}
+
+
+class Item extends React.Component {
+  render() {
+    return (
+      <li>{this.props.name},
+        €{this.props.price}
+      </li>
+
+    )
+
+  }
+
+}
+
+class App extends React.Component {
+
+  state = {
+    items: [
+      { id: 1, name: 'Apple', price: '0.34' },
+      { id: 2, name: 'Orange', price: '0.70' }
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <ul>
+          {this.state.items.map(i => {
+            return (
+              <Item
+                id={i.id}
+                name={i.name}
+                price={i.price}
+              />)
+          })}
+        </ul>
+        <AddForm add={this.add} />
+
+      </div>
+    )
+  }
 }
 
 export default App;
